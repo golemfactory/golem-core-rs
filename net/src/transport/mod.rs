3737f94c02a126cf13b6ea0f3ae6051ec13ed399
Marek Franciszkiewicz <marek@golem.network>
2018-06-28 20:00:37 +0200

    TransportProtocol: int conversion update & tests

--- net/src/transport/mod.rs
+++ net/src/transport/mod.rs
@@ -15,23 +15,9 @@ use super::network::Network;
 /// Available transport protocols
 #[derive(Copy, Clone, Debug, Hash, Eq, PartialEq)]
 pub enum TransportProtocol {
-    Tcp,
-    Udp,
-    Unsupported,
-}
-
-macro_rules! _impl_into {
-    ($($into:ty),+) => {
-        $(impl Into<$into> for TransportProtocol {
-            fn into(self) -> $into {
-                match self {
-                    TransportProtocol::Tcp => 6,
-                    TransportProtocol::Udp => 17,
-                    TransportProtocol::Unsupported => 0,
-                }
-            }
-        })*
-    }
+    Tcp = 6,
+    Udp = 17,
+    Unsupported = 0,
 }
 
 macro_rules! _impl_from {
@@ -48,8 +34,7 @@ macro_rules! _impl_from {
     }
 }
 
-_impl_into!(u8, u16, u32, i8, i16, i32);
-_impl_from!(u8, u16, u32, i8, i16, i32);
+_impl_from!(u16, u32, u64, i16, i32, i64);
 
 
 impl fmt::Display for TransportProtocol {
@@ -87,6 +72,30 @@ where
         match *self {
             TransportSession::Tcp(ref a) => TransportSession::Tcp(a.clone()),
             TransportSession::Udp(ref a) => TransportSession::Udp(a.clone()),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[cfg(test)]
+    mod protocol {
+
+        use transport::TransportProtocol;
+
+        #[test]
+        fn test_as() {
+            assert_eq!(6, TransportProtocol::Tcp as u16);
+            assert_eq!(17, TransportProtocol::Udp as u16);
+            assert_eq!(0, TransportProtocol::Unsupported as u16);
+        }
+
+        #[test]
+        fn test_from() {
+            assert_eq!(TransportProtocol::Tcp, TransportProtocol::from(6));
+            assert_eq!(TransportProtocol::Udp, TransportProtocol::from(17));
+            assert_eq!(TransportProtocol::Unsupported, TransportProtocol::from(0));
         }
     }
 }
