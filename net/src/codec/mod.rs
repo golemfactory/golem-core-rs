f6eaf23b7e394ec44c9ceb2698a9a81b8ca3c0a6
Marek Franciszkiewicz <marek@golem.network>
2018-07-04 16:52:47 +0200

    Peek the length in MessageCodec::decode

--- net/src/codec/mod.rs
+++ net/src/codec/mod.rs
@@ -1,4 +1,4 @@
-use std::u32::MAX as u32_MAX;
+use std::mem;
 
 use bincode::internal::{deserialize, serialize};
 use bincode::Bounded;
@@ -12,6 +12,11 @@ pub mod message;
 use self::error::CodecError;
 use self::message::Message;
 
+type LenType = u32;
+const LEN_SZ: usize = mem::size_of::<LenType>();
+const LEN_MAX: LenType = LenType::max_value();
+
+
 pub struct MessageCodec;
 
 impl Encoder for MessageCodec {
@@ -19,7 +24,7 @@ impl Encoder for MessageCodec {
     type Error = CodecError;
 
     fn encode(&mut self, value: Self::Item, bytes: &mut BytesMut) -> Result<(), Self::Error> {
-        let size_limit = Bounded(u32_MAX as u64);
+        let size_limit = Bounded(LEN_MAX as u64);
         let result = serialize::<Message, Bounded, Order>(&value, size_limit);
         let ser = match result {
             Ok(ser) => ser,
@@ -29,8 +34,8 @@ impl Encoder for MessageCodec {
         };
 
         let ser_ref: &[u8] = ser.as_ref();
-        bytes.reserve(ser_ref.len() + 4);
-        bytes.put_u32_be(ser_ref.len() as u32);
+        bytes.reserve(ser_ref.len() + LEN_SZ);
+        bytes.put_u32_be(ser_ref.len() as LenType);
         bytes.put(ser_ref);
 
         Ok(())
@@ -42,16 +47,16 @@ impl Decoder for MessageCodec {
     type Error = CodecError;
 
     fn decode(&mut self, bytes: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
-        if bytes.len() < 4 {
+        if bytes.len() < LEN_SZ {
             return Ok(None);
         }
 
         let size = BigEndian::read_u32(bytes.as_ref()) as usize;
-
-        bytes.split_to(4);
-        if bytes.len() < size {
+        if bytes.len() < size + LEN_SZ {
             return Ok(None);
         }
+
+        bytes.split_to(LEN_SZ);
 
         let msg = bytes.split_to(size);
         match deserialize::<Message, Order>(msg.as_ref()) {
