20ff2c61a071ed12c4bd90cb50e6e20e85a5126d
Marek Franciszkiewicz <marek@golem.network>
2018-07-04 02:40:00 +0200

    Force convert Encapsulated.message to PyBytes

--- pyext-golem-core/src/event.rs
+++ pyext-golem-core/src/event.rs
@@ -1,4 +1,4 @@
-use cpython::{PyTuple, Python, ToPyObject};
+use cpython::{PyBytes, PyTuple, Python, ToPyObject};
 use std::net::SocketAddr;
 
 use net::codec::message::Encapsulated;
@@ -22,13 +22,16 @@ impl CoreEvent {
     pub fn make_py_tuple(self, py: Python) -> PyTuple {
         match self {
             CoreEvent::Exiting              => py_wrap!(py, (0,)),
-            CoreEvent::Started(t, a)        => py_wrap!(py, (1, t as u16, host_port(&a),)),
-            CoreEvent::Stopped(t, a)        => py_wrap!(py, (2, t as u16, host_port(&a),)),
-            CoreEvent::Connected(t, a, i)   => py_wrap!(py, (100, t as u16, host_port(&a), i,)),
-            CoreEvent::Disconnected(t, a)   => py_wrap!(py, (101, t as u16, host_port(&a),)),
-            CoreEvent::Message(t, a, e)     => py_wrap!(py, (102, t as u16, host_port(&a),
-                                                             (e.protocol_id, e.message),)),
-            CoreEvent::Log(l, m)            => py_wrap!(py, (200, l, m,)),
+            CoreEvent::Started(t, a)        => py_wrap!(py, (1, t as u16, host_port(&a))),
+            CoreEvent::Stopped(t, a)        => py_wrap!(py, (2, t as u16, host_port(&a))),
+            CoreEvent::Connected(t, a, i)   => py_wrap!(py, (100, t as u16, host_port(&a), i)),
+            CoreEvent::Disconnected(t, a)   => py_wrap!(py, (101, t as u16, host_port(&a))),
+            CoreEvent::Message(t, a, e)     => {
+                let bytes = PyBytes::new(py, &e.message[..]);
+                let message = py_wrap!(py, (e.protocol_id, bytes));
+                                               py_wrap!(py, (102, t as u16, host_port(&a), message))
+            },
+            CoreEvent::Log(l, m)            => py_wrap!(py, (200, l, m)),
         }
     }
 }
